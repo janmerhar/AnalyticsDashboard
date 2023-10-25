fe8931caaa940170eb1271ef1f058346117561da
Jan Merhar <merharjan@gmail.com>
2023-10-25 10:53:42 +0200

    Add logger.middleware.ts

---
+++ src/server/src/middleware/logger.middleware.ts
@@ -0,0 +1,24 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(req: Request, res: Response, next: NextFunction): void {
+    const startAt = process.hrtime();
+    const { method, originalUrl } = req;
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const contentLength = res.get('content-length');
+      const diff = process.hrtime(startAt);
+      const responseTime = diff[0] * 1e3 + diff[1] * 1e-6;
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${contentLength} ${responseTime} ms`,
+      );
+    });
+
+    next();
+  }
+}
