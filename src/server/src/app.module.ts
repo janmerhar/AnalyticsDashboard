1dba85bc387ceb4cb93e6e75cec2f2f554684260
Jan Merhar <merharjan@gmail.com>
2023-10-25 10:54:46 +0200

    Add HTTP logging

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -19,4 +20,8 @@ import { MongooseModule } from '@nestjs/mongoose';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
