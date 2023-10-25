1584f273789faa0073130bef200c4eb053841041
Jan Merhar <merharjan@gmail.com>
2023-10-25 09:41:13 +0200

    Add MongoDB connection in app.module.ts

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -3,8 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI,
+      }),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
