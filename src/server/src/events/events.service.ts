1f49506b7367dfa7551ed671bf0e4fd2d3e55e15
Jan Merhar <merharjan@gmail.com>
2023-10-26 10:16:39 +0200

    Add empty events.service.ts

---
+++ src/server/src/events/events.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateEventDto } from './dto/create-event.dto';
+import { SearchEventDto } from './dto/search-event.dto';
+import { Event } from './schemas/event.schema';
+
+import { EventNotFound } from './exceptions/event-not-found.exception';
+import { EventAlreadyExists } from './exceptions/event-already-exists.exception';
+
+@Injectable()
+export class EventsService {
+  private readonly PAGE_SIZE = 12;
+  private readonly logger = new Logger(EventsService.name);
+
+  constructor(
+    @InjectModel(Event.name) private readonly eventModel: Model<Event>,
+  ) {}
+
+}
