0c1e972b2afd843102dbaddcef02b51df3aa4b19
Jan Merhar <merharjan@gmail.com>
2023-10-26 11:31:39 +0200

    Add test setup events.e2e-spec.ts

---
+++ src/server/test/events.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { CreateEventDtoStub } from '../src/events/dto/create-event.dto.stub';
+import { UpdateEventDtoStub } from '../src/events/dto/update-event.dto.stub';
+
+describe('EventsController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('api');
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
