8c148a3d71e3432ba04f8935e9cae7b3b190704c
Jan Merhar <merharjan@gmail.com>
2023-10-27 18:35:39 +0200

    Add fetch event  in events.controller.ts

--- src/server/src/events/events.controller.ts
+++ src/server/src/events/events.controller.ts
@@ -18,6 +18,22 @@ import { Event } from './schemas/event.schema';
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
+
+  @Get(':page?')
+  getEvents(
+    @Param(
+      'page',
+      new ParseIntPipe({
+        optional: true,
+        errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+      }),
+    )
+    page: number | null,
+    @Query() search: SearchEventDto,
+  ): Promise<Event[]> {
+    return this.eventsService.findAll(page || 1, search);
+  }
+
   @Post('/add')
   async postEvent(@Body() body: CreateEventDto): Promise<Event> {
     return this.eventsService.insertOne(body);
