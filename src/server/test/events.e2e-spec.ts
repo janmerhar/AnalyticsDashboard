a7fee3050b6d81076ac5f8658c3fd8d2a28baf31
Jan Merhar <merharjan@gmail.com>
2023-10-27 18:24:58 +0200

    Add fetch event tests in events.e2e-spec.ts

--- src/server/test/events.e2e-spec.ts
+++ src/server/test/events.e2e-spec.ts
@@ -43,6 +43,115 @@ describe('EventsController (e2e)', () => {
     });
   });
 
+  describe('GET /api/events', () => {
+    it('should return array of events', async () => {
+      const result = await request(app.getHttpServer()).get('/api/events');
+
+      expect(result.statusCode).toBe(200);
+      expect(Array.isArray(result.body)).toBe(true);
+    });
+
+    it("filters events by 'type' query param", async () => {
+      const result = await request(app.getHttpServer())
+        .get('/api/events?type=crosspromo')
+        .query({ type: CreateEventDtoStub().type });
+
+      expect(result.statusCode).toBe(200);
+      expect(Array.isArray(result.body)).toBe(true);
+
+      if (result.body.length > 0) {
+        result.body.forEach((event) => {
+          expect(event.type).toBe(CreateEventDtoStub().type);
+        });
+      }
+    });
+
+    it("filters events by 'priority' query param", async () => {
+      const result = await request(app.getHttpServer())
+        .get('/api/events')
+        .query({ priority: CreateEventDtoStub().priority });
+
+      expect(result.statusCode).toBe(200);
+      expect(Array.isArray(result.body)).toBe(true);
+
+      if (result.body.length > 0) {
+        result.body.forEach((event) => {
+          expect(event.priority).toBe(CreateEventDtoStub().priority);
+        });
+      }
+    });
+
+    it("filters events by 'type' and 'priority' query params combined", async () => {
+      const result = await request(app.getHttpServer())
+        .get('/api/events')
+        .query({
+          priority: CreateEventDtoStub().priority,
+          type: CreateEventDtoStub().type,
+        });
+
+      expect(result.statusCode).toBe(200);
+      expect(Array.isArray(result.body)).toBe(true);
+
+      if (result.body.length > 0) {
+        result.body.forEach((event) => {
+          expect(event.priority).toBe(CreateEventDtoStub().priority);
+          expect(event.type).toBe(CreateEventDtoStub().type);
+        });
+      }
+    });
+
+    it("sorts events by 'type' query param", async () => {
+      const result = await request(app.getHttpServer())
+        .get('/api/events')
+        .query({ sort: 'type', order: 'asc' });
+
+      expect(result.statusCode).toBe(200);
+      expect(Array.isArray(result.body)).toBe(true);
+
+      if (result.body.length > 1) {
+        for (let i = 0; i < result.body.length - 1; i++) {
+          const currentEvent = result.body[i];
+          const nextEvent = result.body[i + 1];
+          expect(currentEvent.type <= nextEvent.type).toBe(true);
+        }
+      }
+    });
+
+    it("sorts events by 'priority' query param", async () => {
+      const result = await request(app.getHttpServer())
+        .get('/api/events')
+        .query({ sort: 'priority', order: 'asc' });
+
+      expect(result.statusCode).toBe(200);
+      expect(Array.isArray(result.body)).toBe(true);
+
+      if (result.body.length > 1) {
+        for (let i = 0; i < result.body.length - 1; i++) {
+          const currentEvent = result.body[i];
+          const nextEvent = result.body[i + 1];
+          expect(currentEvent.priority <= nextEvent.priority).toBe(true);
+        }
+      }
+    });
+
+    it('sorts events by name', async () => {
+      const result = await request(app.getHttpServer())
+        .get('/api/events')
+        .query({ sort: 'name', order: 'asc' });
+
+      expect(result.statusCode).toBe(200);
+      expect(Array.isArray(result.body)).toBe(true);
+
+      if (result.body.length > 1) {
+        for (let i = 0; i < result.body.length - 1; i++) {
+          const currentEvent = result.body[i];
+          const nextEvent = result.body[i + 1];
+          expect(currentEvent.name <= nextEvent.name).toBe(true);
+        }
+      }
+    });
+  });
+
   afterAll(async () => {
     await app.close();
   });
