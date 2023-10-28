5261b815fdbb660d532d28af048f4a8529bcec8f
Jan Merhar <merharjan@gmail.com>
2023-10-28 18:18:55 +0200

    Update View.vue

--- src/client/src/layouts/default/View.vue
+++ src/client/src/layouts/default/View.vue
@@ -1,9 +1,26 @@
 <template>
   <v-main>
-    <router-view />
+    <v-container>
+      <v-row>
+        <v-col v-if="!isMobile" md="2" class="pa-0 ma-0"></v-col>
+        <v-col xs="12" md="8">
+          <router-view />
+        </v-col>
+        <v-col v-if="!isMobile" md="2" class="pa-0 ma-0"></v-col>
+      </v-row>
+    </v-container>
   </v-main>
 </template>
 
-<script lang="ts" setup>
-  //
+<script setup lang="ts">
+import { computed } from "vue";
+import { useDisplay } from "vuetify";
+
+const { name } = useDisplay();
+
+
+const isMobile = computed(() => {
+  return name.value === "xs" || name.value === "sm";
+});
+
 </script>
