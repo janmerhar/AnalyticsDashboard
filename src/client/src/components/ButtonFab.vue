2f7f9b649f5a87992621c5725aeab490b091afd9
Jan Merhar <merharjan@gmail.com>
2023-10-28 17:05:40 +0200

    Add ButtonFab.vue

---
+++ src/client/src/components/ButtonFab.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout-item model-value position="bottom" class="text-end" size="100">
+    <div class="ma-5">
+      <VBtn
+        icon="mdi-plus"
+        size="large"
+        color="primary"
+        elevation="10"
+        @click="$emit('add')"
+      />
+    </div>
+  </v-layout-item>
+</template>
+
+<script setup lang="ts">
+defineEmits(["add"]);
+</script>
