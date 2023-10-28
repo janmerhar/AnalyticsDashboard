c57db556c9b13fd04e14a385650036cf5b79f4ee
Jan Merhar <merharjan@gmail.com>
2023-10-28 19:54:29 +0200

    Add events.ts

---
+++ src/client/tests/Event/events.ts
@@ -0,0 +1,74 @@
+import { EventDetails } from "../../src/entities/Event";
+
+export const events = [
+  {
+    name: "Event 1",
+    description: "Event 1 description",
+    type: "app",
+    priority: 10,
+    id: 4,
+  },
+  {
+    name: "Event 2",
+    description: "Event 1 description",
+    type: "app",
+    priority: 10,
+    id: 5,
+  },
+  {
+    name: "Event 3",
+    description: "Event 1 description",
+    type: "ads",
+    priority: 10,
+    id: 6,
+  },
+  {
+    name: "Event 1",
+    description: "Event 1 description",
+    type: "app",
+    priority: 10,
+    id: 65,
+  },
+  {
+    name: "Event 1",
+    description: "Event 1 description",
+    type: "app",
+    priority: 10,
+    id: 69,
+  },
+  {
+    name: "Event 1",
+    description: "Event 1 description",
+    type: "ads",
+    priority: 10,
+    id: 78,
+  },
+  {
+    name: "Event 1",
+    description: "Event 1 description",
+    type: "app",
+    priority: 10,
+    id: 77,
+  },
+  {
+    name: "Event 1",
+    description: "Event 1 description",
+    type: "app",
+    priority: 10,
+    id: 67,
+  },
+  {
+    name: "Event 1",
+    description: "Event 1 description",
+    type: "app",
+    priority: 10,
+    id: 87,
+  },
+  {
+    name: "Event 1",
+    description: "Event 1 description",
+    type: "app",
+    priority: 10,
+    id: 88,
+  },
+] as EventDetails[];
