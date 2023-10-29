98bc5f202a8215d71740ae8960acc19e7bbf136a
Jan Merhar <merharjan@gmail.com>
2023-10-29 15:24:38 +0100

    Add CORS settings in NestJS main.ts

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -4,8 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api');
+  app.enableCors({
+    methods: 'GET,POST,PATCH,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
